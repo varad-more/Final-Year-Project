615cf4516dd1e58458fc8a3635742728b90adb3a
sonal <smsarode1@gmail.com>
2020-06-10 16:35:09 +0530

    modified speech recognition file

--- speech_recognition.py
+++ speech_recognition.py
@@ -2,49 +2,55 @@ import pymongo
 import datetime
 import speech_recognition as sr 
 import pyttsx3
-
-def Speak(text):
+#text to speech speaking
+'''def Speak(text):
     engine = pyttsx3.init()
     engine.say(text)
-    engine.runAndWait()
-# Initialize the recognizer  
-while(1):
-    r = sr.Recognizer()  
-    try:
-        print("speek...")
-        with sr.Microphone() as source:
+    engine.runAndWait()'''
     
-            r.adjust_for_ambient_noise(source, duration=0.2) 
-            audio = r.listen(source)
-            text = r.recognize_google(audio)
-            print("you spoke.... :{}".format(text)) 
-            y = "{}".format(text)
-            Speak(text)
-            #connect_database()
-    except:
-
-        print("sorry we could no recognize your voice")
-
-
-def connect_database():
+def demodatabase(y):
     client =  pymongo.MongoClient('mongodb://localhost:27017/')
-    db = client['demodb']
-    mycol = db["prescription"]
+    db = client['demodb'] #database name
+    mycol = db["prescription"]#collection list 
     print (mycol)
     collist = db.list_collection_names()
     if "reports" in collist:
           print("The collection exists.")
 
-
-    print(1)
     print("The collection exists.")
-    print(2)
     post = {"name": y,
             "power": "500g",
             "time": datetime.datetime.utcnow()}
-    print(3)
     x = mycol.insert_one(post)
     print(x)
-    print(4)
     
-connect_database()
+
+# Initialize the recognizer  
+
+r = sr.Recognizer()
+m = sr.Microphone()
+try:
+    
+    with m as source: 
+        r.adjust_for_ambient_noise(source)
+        print("Set minimum energy threshold to {}".format(r.energy_threshold))
+        print("Say something!")
+        audio = r.listen(source)
+        text = r.recognize_google(audio)
+        print('{}'.format(text))
+        y = "{}".format(text)
+        #Speak(text)
+        demodatabase(y)
+            #connect_database()
+except sr.UnknownValueError:
+    print("Oops! Didn't catch that")
+except sr.RequestError as e:
+    print("Uh oh! Couldn't request results from Google Speech Recognition service; {0}".format(e))      
+except KeyboardInterrupt:
+    pass
+
+
+
+
+ 
+
