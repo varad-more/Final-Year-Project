356ad1923ee18d3671e54b09f05ec0c6942d4d9a
varad-more <varadmore1@gmail.com>
2021-01-26 18:54:57 +0530

    Dashboard cleanup

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Final-Year-Project
 
 This is our final year project
+
+## Setup Instruction 
+Install following dependencies of the project
+```
+sudo apt-get install python3-dev default-libmysqlclient-dev build-essential 
+pip install mysqlclient numpy tabula-py django twilio jsonfield mysql-connector sentry_sdk
+```
--- dashboard/medibot/medibot/settings.py
+++ dashboard/medibot/medibot/settings.py
@@ -11,6 +11,18 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://318377cdb44d452ea17a8da7c9574821@o497739.ingest.sentry.io/5574354",
+    integrations=[DjangoIntegration()],
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
